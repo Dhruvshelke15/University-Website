fc1040260066a211e2d0fae8396de4be9d9bb0a2
Dhruvshelke15 <Dhruvshelke588@gmail.com>
2022-10-19 00:10:28 +0530

    first commit

---
+++ feedback.php
@@ -0,0 +1,66 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>University Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,700;1,500&display=swap" rel="stylesheet">
+<script src="https://use.fontawesome.com/4976a87626.js"></script>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$name = $email = $gender = $comment = $website = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $website = test_input($_POST["website"]);
+  $comment = test_input($_POST["comment"]);
+  $gender = test_input($_POST["gender"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+<div class="containerfeeb">
+<h2>PHP Form Validation Example</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">  
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Website: <input type="text" name="website">
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+</div>
+</body>
+</html>
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.t0ntt.mongodb.net/details")
+
+// schema
+const signupSchema= {
+    email: String,
+    username: String,
+    password: String,
+    dateofbirth: String
+}
+
+const SignUp = mongoose.model("SignUp", signupSchema);
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/login.html");
+})
+
+app.post("/", function(req, res){
+    let newSignUp = new SignUp({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        dateofbirth: req.body.dateofbirth
+    });
+    newSignUp.save();
+    res.redirect('/');
+})
+
+app.listen(3000, function(){
+    console.log("server is running on 3000");
+})
